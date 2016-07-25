9eccc386710c0fc7a360a5f1bbe3e169305eae3e
Andrew <andrew.mecham@gmail.com>
2016-07-25 08:55:18 -0600

    updated files

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,28 @@
-angular.module("getInfo", [ui.router])
+angular.module("getInfo", ["ui.router"])
     .config(function($stateProvider, $urlRouterProvider) {
             $stateProvider
 
                 .state('home', {
                     url: '/',
                     templateUrl: "HTML/main.html"
-                }),
+                })
 
                 .state('quotes',{
-                    url: '/quotes',
-                    templateUrl:"HTML/quotes.html"
-                }),
+               url:'/quotes',
+               parent:'home',
+               templateUrl: "HTML/quotes.html"
+           })
 
                 .state('weather',{
                     url: '/weather',
-                    templateUrl:"HTML/weather.html"
-                }),
+                    templateUrl: "HTML/weather.html"
+                })
 
                 .state('info',{
                     url: '/info',
-                    templateUrl:"HTML/info.html"
+                    templateUrl: "HTML/info.html"
                 })
 
                 $urlRouterProvider
             .otherwise('/');
+})
